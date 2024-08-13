4dcc5f160d8735e90920f21f558187046ed92198
termilu <47648082+termilu@users.noreply.github.com>
2024-08-13 05:03:53 +0200

    Removed unused imports

--- src/main/java/net/termilu/termc/datagen/ModLootTableGenerator.java
+++ src/main/java/net/termilu/termc/datagen/ModLootTableGenerator.java
@@ -8,7 +8,6 @@ import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
