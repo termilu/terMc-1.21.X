2ee1af05dd231c28cd8253f142443c39e871d4b4
termilu <47648082+termilu@users.noreply.github.com>
2024-09-25 17:18:57 +0200

    Add custom TermBowItem with increased damage
    - Created TermBowItem class extending BowItem
    - Overridden shoot method to increase the damage of arrows by 1.5 times
    - Registered TERM_BOW item in ModItems class
    - Added model predicates for TERM_BOW in ModModelPredicates class

---
+++ src/main/java/net/termilu/termc/item/custom/TermBowItem.java
@@ -0,0 +1,20 @@
+package net.termilu.termc.item.custom;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.BowItem;
+
+public class TermBowItem extends BowItem {
+    public TermBowItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected void shoot(LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, LivingEntity target) {
+        if (projectile instanceof ArrowEntity) {
+            ((ArrowEntity) projectile).setDamage(((ArrowEntity) projectile).getDamage() * 1.5); // Double the damage
+        }
+        super.shoot(shooter, projectile, index, speed, divergence, yaw, target);
+    }
+}
--- src/main/java/net/termilu/termc/util/ModModelPredicates.java
+++ src/main/java/net/termilu/termc/util/ModModelPredicates.java
@@ -1,10 +1,13 @@
 package net.termilu.termc.util;
 
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.termilu.termc.TerMc;
 import net.termilu.termc.components.ModDataComponentTypes;
 import net.termilu.termc.item.ModItems;
+
+import static net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry.register;
 
 public class ModModelPredicates {
     //Predicates register in TerMcClient
@@ -13,5 +16,23 @@ public class ModModelPredicates {
         //Change texture of data tablet depending on predicate on or off
         ModelPredicateProviderRegistry.register(ModItems.DATA_TABLET, Identifier.of(TerMc.MOD_ID, "on"),
                 (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.FOUND_BLOCK) != null ? 1f : 0f);
+
+        registerCustomBow(ModItems.TERM_BOW);
+    }
+
+    //Taken from ModelPredicateProviderRegistry
+    private static void registerCustomBow(Item item){
+        register(item, Identifier.ofVanilla("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return entity.getActiveItem() != stack ? 0.0F : (float)(stack.getMaxUseTime(entity) - entity.getItemUseTimeLeft()) / 20.0F;
+            }
+        });
+        register(
+                item,
+                Identifier.ofVanilla("pulling"),
+                (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getActiveItem() == stack ? 1.0F : 0.0F
+        );
     }
 }
